95e574f813e112070d1d464927c0694c94fb1a0a
Tobi Ijiyemi <tobi.ijiyemi@gmail.com>
2024-04-05 23:21:09 -0400

    switched up vercel json again

--- social_networking_app/social_networking_app/settings.py
+++ social_networking_app/social_networking_app/settings.py
@@ -38,8 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'login',
-    'django_nextjs',
-    #'django_nextjs.apps.DjangoNextJSConfig'
 ]
 
 MIDDLEWARE = [
