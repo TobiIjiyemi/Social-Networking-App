19fe2bd481e08e5d23826f85bed2428193f6a5bb
Tobi Ijiyemi <tobi.ijiyemi@gmail.com>
2024-04-05 23:50:23 -0400

    got rid of url patterns

--- social_networking_app/social_networking_app/urls.py
+++ social_networking_app/social_networking_app/urls.py
@@ -24,5 +24,3 @@ urlpatterns = [
     path("login/", include("login.urls")),
     path('admin/', admin.site.urls)
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
