ad86caefad2d69fd63158b594fb31b086dcd14b1
RachelLi15 <146363155+RachelLi15@users.noreply.github.com>
2024-04-11 12:38:46 -0400

    added config

--- firebase_auth/views.py
+++ firebase_auth/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+import pyrebase
+ 
+config={
+  "apiKey": "AIzaSyD28otVLAP-g3GL_Avt4uS8TyhWqoiU8bY",
+  "authDomain": "socialapp-34c1e.firebaseapp.com",
+  "databaseURL": "https://socialapp-34c1e-default-rtdb.firebaseio.com",
+  "projectId": "socialapp-34c1e",
+  "storageBucket": "socialapp-34c1e.appspot.com",
+  "messagingSenderId": "48387053736",
+  "appId": "1:48387053736:web:27ca5f78b748529ddcca9d",
+  "measurementId": "G-LQS2EGEHQ8"
+}
+firebase=pyrebase.initialize_app(config)
+authe = firebase.auth()
+database=firebase.database()
+
+print(database.child('User').get().val())
